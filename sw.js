d693998461e7de779e936a488e1b77683b03a14a
branzfamily01 <branz.family.01@gmail.com>
2026-08-27 07:55:22 +0900

    Complete manual and My Hub metadata (#3)
    * Add in-app manual entry point
    
    * Cache manual for offline use
    
    * Add My Hub metadata

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='library-reference-navigator-v2';
-const ASSETS=['./','./index.html','./css/app.css','./js/prototype-data.js','./js/app.js','./js/search-worker.js','./js/csv-worker.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='library-reference-navigator-v3';
+const ASSETS=['./','./index.html','./manual.html','./css/app.css','./js/prototype-data.js','./js/app.js','./js/search-worker.js','./js/csv-worker.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
